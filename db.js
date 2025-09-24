62412a3c3536164b2704edd5db7ca9c9d382addb
ang626 <gg58306760@gmail.com>
2025-09-24 21:01:43 +0900

    test

--- db.js
+++ db.js
@@ -1,32 +1,17 @@
-const mysql = require('mysql2');
+const { Client } = require('pg');
 
 // MariaDB 연결
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234', // 본인 DB 비밀번호
-    database: 'schedule_db'
+const client = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS, // 본인 DB 비밀번호
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
 });
 
-db.connect(err => {
-    if (err) throw err;
-    console.log('MariaDB 연결 성공!');
+client.connect()
+  .then(() => console.log("✅ PostgreSQL Connected!"))
+  .catch(err => console.error("❌ Connection error", err.stack));
 
-    // 테이블 생성
-    const createTableQuery = `
-        CREATE TABLE IF NOT EXISTS schedules (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            title VARCHAR(255) NOT NULL,
-            memo TEXT,
-            date DATE NOT NULL,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        );
-    `;
-    db.query(createTableQuery, (err) => {
-        if (err) throw err;
-        console.log('schedules 테이블 생성 완료 또는 이미 존재함');
-    });
-});
-
-module.exports = db;
+module.exports = client;
